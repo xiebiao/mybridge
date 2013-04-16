2c4a2e6c78e41ebedfd19a00b286ac00a3c992b0
xiebiao <joyrap@gmail.com>
2013-04-16 22:58:57 +0800

    update test

--- src/test/java/mybridge/test/TestJdbc.java
+++ src/test/java/mybridge/test/TestJdbc.java
@@ -1,26 +1,49 @@
 package mybridge.test;
 
 import java.sql.Connection;
+import java.sql.Driver;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
 
+import com.alibaba.druid.pool.DruidDataSource;
+
 public class TestJdbc {
 	private static org.slf4j.Logger LOG = org.slf4j.LoggerFactory
 			.getLogger(TestJdbc.class);
+	private static DruidDataSource ds;
+	static {
+		try {
+			Driver driver = (Driver) Class.forName("com.mysql.jdbc.Driver")
+					.newInstance();
+			ds = new DruidDataSource();
+			ds.setDriver(driver);
+			ds.setUrl("jdbc:mysql://127.0.0.1:3307/wp");
+			ds.setUsername("root");
+			ds.setPassword("yes");
+			ds.setTestOnBorrow(false);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 
 	public static void main(String[] args) throws Exception {
+		int count = 10;
+		while (count-- > 0) {
+			query(count);
+		}
+	}
 
-		Class.forName("com.mysql.jdbc.Driver");
-		Connection conn = DriverManager.getConnection(
-				"jdbc:mysql://127.0.0.1:3307/wp", "root", "yes");
+	public static void query(int i) throws Exception {
+		Connection conn = ds.getConnection();
 		try {
 			LOG.debug("查询结果");
 			Statement stmt = conn.createStatement();
 			ResultSet rs = stmt.executeQuery("select * from wp_users");
 			while (rs.next()) {
-				LOG.debug(rs.getString(1) + " " + rs.getString(2) + " "
-						+ rs.getString(3));
+				LOG.debug("select " + i + " :" + rs.getString(1) + " "
+						+ rs.getString(2) + " " + rs.getString(3));
 
 			}
 			conn.close();
@@ -28,5 +51,4 @@ public class TestJdbc {
 			e.printStackTrace();
 		}
 	}
-
 }
