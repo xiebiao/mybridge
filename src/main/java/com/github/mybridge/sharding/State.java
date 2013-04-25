369882f5989bc4512ff6e0fc295ae8841ffa1c4b
xiebiao <joyrap@gmail.com>
2013-04-25 21:10:33 +0800

    Update State comment

--- src/main/java/com/github/mybridge/sharding/State.java
+++ src/main/java/com/github/mybridge/sharding/State.java
@@ -1,8 +1,15 @@
 package com.github.mybridge.sharding;
 
-
 /**
  * 读写状态
+ * <p>
+ * 实现类可能存在三种状态：
+ * <ul>
+ * <li>1. 只可写</li>
+ * <li>2. 只可读</li>
+ * <li>3. 可读写</li>
+ * </ul>
+ * </p>
  * 
  * @author xiebiao
  */
@@ -10,16 +17,14 @@ public interface State {
 	/**
 	 * 可写入
 	 * 
-	 * @param node
-	 * @return
+	 * @return true:可写,false:不可写
 	 */
 	public boolean canWrite();
 
 	/**
 	 * 可读取
 	 * 
-	 * @param node
-	 * @return
+	 * @return true:可读,false:不可读
 	 */
 	public boolean canRead();
 }
